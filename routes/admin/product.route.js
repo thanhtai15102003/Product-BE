e4b990b519bbc608113c407a3ff27e7f67746d6d
thanhtai15102003 <thainguyenthanhtai2003@gmail.com>
2026-01-14 13:18:43 +0700

    Push Cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const multer = require('multer');
-const storageMulter = require('../../helpers/storageMulter');
-const upload = multer({ storage: storageMulter() });
+
+const upload = multer();
 const router = express.Router();
 const controller = require('../../controllers/admin/product.controller');
 const validate = require('../../validates/admin/product.validate');
+const uploadCloud = require('../../middlewares/admin/uploadCloud.middleware');
 
 router.get('/', controller.index);
 router.patch('/change-status/:status/:id', controller.changeStatus);
 router.patch('/change-multi', controller.changeMulti);
 router.delete('/delete/:id', controller.deleteItem);
 router.get('/create', controller.create);
-router.post('/create',
+router.post(
+    '/create',
     upload.single('thumbnail'),
+    uploadCloud.upload,
     validate.createPost,
     controller.createPost
 );
